4fb427c79458efd82f8fc960c82379ebb9c4f7b8
ilyesgasmi <belkacemilyesgasmi@gmail.com>
2024-01-28 11:11:21 -0500

    nice cute code

--- run_model.py
+++ run_model.py
@@ -1,86 +1,76 @@
 import cv2
+import requests
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import requests
 
 # Load the TensorFlow model
 model = tf.saved_model.load('./')
-classes = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
-frame_width = 640
-frame_height = 480
+classes = ["paper", "metal", "cardboard", "trash", "glass", "plastic", "not trash"]
 
-# Region of interest
-roi_width = int(frame_width * 2 / 3)    # 2/3 of the frame's width
-roi_height = int(frame_height * 2 / 3)  # 2/3 of the frame's height
-roi_x = 0
-roi_y = frame_height - roi_height       # Start from 2/3 height from the top
+# Create a VideoCapture object to capture video from the camera
+cap = cv2.VideoCapture(0)
 
-
-# Start video capture from the second camera (index 1)
-cap = cv2.VideoCapture(1)
-
-# Check if the camera opened successfully
-if not cap.isOpened():
-    print("Error: Camera could not be opened.")
-    cap.release()
-    exit()
-
+counter = 0
+last_prediction = None
+last_found = None
 while True:
+    # Read the frame from the camera
     ret, frame = cap.read()
-    if not ret:
-        print("Error: Failed to capture an image from the camera.")
-        break
 
-    cv2.rectangle(frame, (roi_x, roi_y), (roi_x + roi_width, roi_y + roi_height), (0, 255, 0), 2)
-    
-    # Display the frame
-    cv2.imshow('Frame', frame)
+    # Convert the frame to RGB format
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Press 'c' to capture an image and process it
-    if cv2.waitKey(1) & 0xFF == ord('c'):
-        roi = frame[roi_y:roi_y+roi_height, roi_x:roi_x+roi_width]
+    # Resize the frame to match the input size of the model
+    img = Image.fromarray(rgb_frame)
+    img = img.resize((300, 300 * img.size[1] // img.size[0]))
 
-        # Convert the captured frame to PIL Image
-        img = Image.fromarray(roi)
-        img = img.resize((300, 300 * img.size[1] // img.size[0]), Image.LANCZOS)
-        
-        # Save image
-        img.save('test.jpg')
-        inp_numpy = np.array(img)[None]
+    # Convert the image to a NumPy array
+    inp_numpy = np.array(img)[None]
 
-        # Prepare input for the model
-        inp = tf.constant(inp_numpy, dtype='float32')
+    # Create a TensorFlow constant from the input array
+    inp = tf.constant(inp_numpy, dtype='float32')
 
-        # Get class scores from the model and find the prediction
-        class_scores = model(inp)[0].numpy()
-        prediction = classes[class_scores.argmax()]
+    # Get the class scores from the model
+    class_scores = model(inp)[0].numpy()
 
-        print("class_scores", class_scores)
-        print("Class : ", prediction)
+    # Get the predicted class label
+    predicted_class = classes[class_scores.argmax()]
 
-        # Construct the URL based on the prediction
-        url = "http://192.168.55.75/"
-        if prediction in ["cardboard", "glass", "paper", "plastic"]:
-            url += "1"
-        elif prediction == "metal":
-            url += "2"
-        elif prediction == "trash":
-            url += "3"
+    # Display the frame and predicted class label
+    cv2.putText(frame, f"Class: {predicted_class}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    cv2.imshow('Camera', frame)
+    if last_prediction == predicted_class:
+        counter += 1
+    else:
+        counter = 0
+    if counter == 20 and last_found != predicted_class:
+        last_found = predicted_class
+        print(f"Found {predicted_class}")
+        if not predicted_class == "not trash":
+            # send request to arduino:
+            # Construct the URL based on the prediction
+            url = "http://192.168.55.75/"
+            if predicted_class in ["cardboard", "glass", "paper", "plastic"]:
+                url += "1"
+            elif predicted_class == "metal":
+                url += "2"
+            elif predicted_class == "trash":
+                url += "3"
+            
+            # Make a GET request to the web server
+            response = requests.get(url)
+            # Check if the request was successful and print the response
+            if response.status_code == 200:
+                print("Message sent successfully")
+            else:
+                print(f"Failed to send message. Status code: {response.status_code}")
 
-        # Make a GET request to the web server
-        response = requests.get(url)
-
-        # Check if the request was successful and print the response
-        if response.status_code == 200:
-            print("Message sent successfully")
-        else:
-            print(f"Failed to send message. Status code: {response.status_code}")
-
-    # Press 'q' to quit
+    last_prediction = predicted_class
+    # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close any OpenCV windows
+# Release the VideoCapture object and close the windows
 cap.release()
 cv2.destroyAllWindows()
